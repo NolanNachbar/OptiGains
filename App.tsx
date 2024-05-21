3e593e551e7f970737df0759dd649695efa427f1
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-05-20 21:21:43 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,21 +3,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import StartWorkoutScreen from './StartWorkoutScreen';
+import CreateWorkoutScreen from './CreateWorkoutScreen';
 import ViewProgressScreen from './ViewProgressScreen';
 import SettingsScreen from './SettingsScreen';
+import { DarkModeProvider } from './DarkModeContext';
 
 const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="StartWorkout" component={StartWorkoutScreen} />
-        <Stack.Screen name="ViewProgress" component={ViewProgressScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <DarkModeProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="StartWorkout" component={StartWorkoutScreen} />
+          <Stack.Screen name="CreateWorkout" component={CreateWorkoutScreen} />
+          <Stack.Screen name="ViewProgress" component={ViewProgressScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </DarkModeProvider>
   );
 };
 
